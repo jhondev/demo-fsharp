fc0c06fd4ccb02760f31c2e4cdf77d1c59f6b30f
Jhonattan <jma9974@gmail.com>
2020-03-18 22:58:42 -0500

    init

---
+++ Program.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.org
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#! edited"
+    printfn "%A %d" argv argv.Length
+    0 // return an integer exit code
